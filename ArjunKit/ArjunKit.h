fc4230991461f68851c8a02f10a74d71f746a758
Arjun T Raj <arjun.thanniyanickal@rakbank.ae>
2023-11-15 11:00:06 +0400

    initial commit

---
+++ ArjunKit/ArjunKit.h
@@ -0,0 +1,18 @@
+//
+//  ArjunKit.h
+//  ArjunKit
+//
+//  Created by Arjun T Raj on 14/11/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ArjunKit.
+FOUNDATION_EXPORT double ArjunKitVersionNumber;
+
+//! Project version string for ArjunKit.
+FOUNDATION_EXPORT const unsigned char ArjunKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArjunKit/PublicHeader.h>
+
+
---
+++ ArjunKit/RakSdk.swift
@@ -0,0 +1,20 @@
+//
+//  RakSdk.swift
+//  ArjunKit
+//
+//  Created by Arjun T Raj on 14/11/2023.
+//
+
+import Foundation
+import UIKit
+
+public class RakSdk {
+    public init(){}
+    public func getUniqueDevcieId()-> String{
+        return "Hello Arjun"
+    }
+    
+    public func getTime()-> String{
+        return " Hello getTime"
+    }
+}
